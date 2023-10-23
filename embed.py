4e8c4ffa6d61d67de1b6b0b4556427bb28d2a422
Your Name <matthewpaullewis@gmail.com>
2023-10-23 19:59:59 -0400

    Embeddings

--- embed.py
+++ embed.py
@@ -1,8 +1,10 @@
 import cohere
-# import pandas as pd
-# import numpy as np
-# import altair as alt
-# import textwrap as tr
+import pandas as pd
+import numpy as np
+import altair as alt
+import textwrap as tr
+import matplotlib.pyplot as plt
+import umap
 
 api_key = 'cUkUMhISEr8QsUhZ8uaVMxZtdL3UJrlaESCyNtHR'
 co = cohere.Client(api_key)
@@ -19,3 +21,38 @@ def embed_text(texts):
   return embedding
 
 df["search_term_embeds"] = embed_text(df["search_term"].tolist())
+embeds = list(df["search_term_embeds"])
+
+reducer = umap.UMAP(n_neighbors=49)
+umap_embeds = reducer.fit_transform(embeds)
+
+df['x'] = umap_embeds[:,0]
+df['y'] = umap_embeds[:,1]
+
+chart = alt.Chart(df).mark_circle(size=500).encode(
+  x=
+  alt.X('x',
+      scale=alt.Scale(zero=False),
+      axis=alt.Axis(labels=False, ticks=False, domain=False)
+  ),
+
+  y=
+  alt.Y('y',
+      scale=alt.Scale(zero=False),
+      axis=alt.Axis(labels=False, ticks=False, domain=False)
+  ),
+  
+  tooltip=['search_term']
+  )
+
+text = chart.mark_text(align='left', dx=15, size=12, color='black'
+          ).encode(text='search_term', color= alt.value('black'))
+
+result = (chart + text).configure(background="#FDF7F0"
+      ).properties(
+      width=1000,
+      height=700,
+      title="2D Embeddings"
+      )
+
+result.save('embed-chart.html')
